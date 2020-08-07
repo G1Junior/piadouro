427e0a52e141fd56d35bf02702793e1bb304be7d
Junior <yanhadlich@gmail.com>
2020-08-06 21:17:08 -0300

    adiciona sistema de follow

--- pessoa/urls.py
+++ pessoa/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from pessoa.views import Home, UserList
+from pessoa.views import Home, UserList, Follow
 
 urlpatterns = [
     path('usuarios/', UserList.as_view(), name='usuarios'),
+    path('follow/<int:profile_id>', Follow.as_view(), name='follow'),
     path('<username>/', Home.as_view(), name='perfil'),
 ]
--- pessoa/views.py
+++ pessoa/views.py
@@ -1,8 +1,9 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.base import RedirectView
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
-
+from pessoa.models import Perfil
 
 class Home(DetailView):
     template_name = 'home.html'
@@ -14,4 +15,17 @@ class Home(DetailView):
 
 class UserList(ListView):
     template_name = 'user_list.html'
-    model = User
+    model = Perfil
+
+class Follow(RedirectView):
+    permanent = False
+    query_string = False
+    pattern_name = 'usuarios'
+
+    def get_redirect_url(self, *args, **kwargs):
+        profile_to_follow = get_object_or_404(Perfil, pk=kwargs['profile_id'])
+        if self.request.user.perfil.seguindo.filter(pk=profile_to_follow.pk).exists():
+            self.request.user.perfil.seguindo.remove(profile_to_follow)
+        else:
+            self.request.user.perfil.seguindo.add(profile_to_follow)
+        return super().get_redirect_url()
